00efe683caf362acd3825f0c8c358332f3f35565
Edward Slavov <e.slavov11@gmail.com>
2016-11-22 23:24:36 +0200

    added new funcitons to datamanip

--- 1.1 C++ Exercises/data_manip.cpp
+++ 1.1 C++ Exercises/data_manip.cpp
@@ -3,6 +3,33 @@
 */
 
 /**
+ * strtoul(tokens[i].c_str(), NULL, 10); Convert string to unsigned long long
+ *
+*/
+
+/**
+ * removes equal consecutive chars ex. aaabb -> ab
+ */
+char *removeEqualConsecutiveChars(char *input) {
+    size_t stringLength = strlen(input);
+    char *cpy = malloc(stringLength);
+    cpy[0] = input[0];
+
+    int i, index = 0;
+    for (i = 0; i < stringLength; i++) {
+        if (input[i] != input[i - 1]) {
+            cpy[index] = input[i];
+            index++;
+        }
+    }
+
+    cpy[index] = '\0';
+    strcpy(input, cpy);
+
+    return input;
+}
+
+/**
  * Splits into vector string by delimiter
  * @param theStringVector - the vector in which the tokens will be pushed
  * @param theString
@@ -20,6 +47,105 @@ void splitString(vector <string> &theStringVector, const string &theString, cons
         start = ((end > (string::npos - theDelimiter.size()))
                  ? string::npos : end + theDelimiter.size());
     }
+}
+
+/**
+ * pad string right with chars
+ * example: ("123", '0', '6') -> "123000"
+ */
+char *padRight(char *input, char paddingChar, int totalStringSize) {
+    //doesnt work with big values over 40
+    size_t stringLength = strlen(input);
+
+    char *cpy = malloc(totalStringSize + 1);
+
+    if (stringLength < totalStringSize) {
+        strcpy(cpy, input);
+        int i;
+        for (i = stringLength; i < totalStringSize; i++) {
+            cpy[i] = paddingChar;
+        }
+        cpy[totalStringSize] = '\0';
+        strcpy(input, cpy);
+    }
+
+    return input;
+}
+
+/**
+ * pad string left with chars
+ * example: ("123", '0', '6') -> "000123"
+ */
+char *padLeft(char *input, char paddingChar, int totalStringSize) {
+    //doesnt work with big values over 40
+    size_t stringLength = strlen(input);
+
+    char *cpy = malloc(totalStringSize + 1);
+
+    if (stringLength < totalStringSize) {
+        strcpy(cpy, input);
+        int i, j;
+        for (i = 0; i < totalStringSize - stringLength; i++) {
+            cpy[i] = paddingChar;
+        }
+        for (i = totalStringSize - stringLength, j = 0; i < totalStringSize; i++) {
+            cpy[i] = input[j++];
+        }
+
+        cpy[totalStringSize] = '\0';
+        strcpy(input, cpy);
+    }
+
+    return input;
+}
+
+/**
+ * Usage - (00001001, 2) -> 00100100
+*/
+long long *rotateLeft(long long nums[], long long positions) {
+    //not fully tested, rotating bits, works with 64bits signed
+    size_t i;
+    positions = positions % 64;
+    for (i = 0; i < sizeof(nums); i++) {
+        long long mask = pow(2, 63 - positions) - 1; // 64 bits mask
+        mask = nums[i] & mask;
+        nums[i] <<= positions;
+        nums[i] |= mask >> (positions);
+    }
+
+}
+
+/**
+ * Usage - (00001001, 2) -> 01000010
+*/
+long long *rotateRight(long long nums[], long long positions) {
+    //not fully tested, rotating bits, works with 64bits signed
+    size_t i;
+    positions = positions % 64;
+    for (i = 0; i < sizeof(nums); i++) {
+        long long mask = pow(2, positions) - 1; //64 bits
+        mask = nums[i] & mask;
+        nums[i] >>= positions;
+        nums[i] |= mask << (63 - positions);
+    }
+}
+
+void printBinary(long long number) {
+    int bit;
+    for (bit = 64 - 1; bit >= 0; --bit) {
+        if ((number >> bit) & 1) {
+            break;
+        }
+
+        printf("0");
+    }
+
+    while (bit >= 0) {
+        printf("%d", ((number >> bit) & 1));
+        --bit;
+    }
+
+    printf("\n");
 }
 
 int findSmallestDigit(unsigned long long number) {
@@ -49,10 +175,3 @@ int findLargestDigit(unsigned long long number) {
 
     return smallestDigit;
 }
-
-
-/**
- * HOW TO's
- * strtoul(tokens[i].c_str(), NULL, 10); Convert string to unsigned long long
- *
-*/
